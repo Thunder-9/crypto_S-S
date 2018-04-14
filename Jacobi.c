95d043f8e6f150b912df4dd4bf70b727878da6dc
Tristan <tristan.bessac@live.fr>
2018-04-14 20:13:22 +0200

    petite correction et commentaire

--- Jacobi.c
+++ Jacobi.c
@@ -3,15 +3,15 @@
 
 int jacobi(mpz_t n, mpz_t m){
 //m=m mod m   etape 1
-	pz_t tmp;
+	mpz_t tmp;
 	mpz_init(tmp);
 	mpz_mod(m,m,n);
 	if(mpz_mod_ui(tmp,m,2)==0){mpz_set(m,etape2(m,n));} //etape2
-	if (mpz_cmp_d(m,1)==0){return 1;}
-	if (mpz_cmp_d(pgcd(m,n),1)!=0){return 0;}
-	if ((mpz_cmp_d(mpz_mod_ui(tmp,n,4),1)==0) || (mpz_cmp_d(mpz_mod_ui(tmp,m,4),1)==0)){
+	if (mpz_cmp_d(m,1)==0){return 1;}  //si m==1 return 1
+	if (mpz_cmp_d(pgcd(m,n),1)!=0){return 0;} //si m,n pas premier entre eux return 0
+	if ((mpz_cmp_d(mpz_mod_ui(tmp,n,4),1)==0) || (mpz_cmp_d(mpz_mod_ui(tmp,m,4),1)==0)){ //etape 4 recursive avec inversement
 	return jacobi(m,n);}
-	else {return -1*jacobi(m,n);}	
+	else {return(-1*jacobi(m,n));}	
 }
 		
 
